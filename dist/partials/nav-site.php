3b95ca7728e4b26c35541ee3c5352dae356ffc9c
Inggo <inggo.espinosa@gmail.com>
2023-05-15 17:58:47 +0800

    Front-page fixes

--- dist/partials/nav-site.php
+++ dist/partials/nav-site.php
@@ -1,3 +1,16 @@
+<?php
+
+class PSBA_Nav_Menu extends Walker_Nav_Menu
+{
+    public function end_el(&$output, $data_object, $depth = 0, $args = null) {
+        if ($depth > 0 && isset($data_object->classes) && in_array("menu-item-has-children", $data_object->classes)) {
+            $output .= "<button class=\"sub-menu-toggle btn btn-primary\">&plus;</button>";
+        }
+        parent::end_el($output, $data_object, $depth, $args);
+    }   
+}
+
+?>
 <nav class="navbar navbar-expand-lg navbar-dark align-self-center">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,6 +22,7 @@
         'container_class' => 'collapse navbar-collapse',
         'container_id'    => 'main-menu',
         'menu_class'      => 'navbar-nav mr-auto ml-auto',
+        'walker'          => new PSBA_Nav_Menu()
     ));
     ?>
 </nav>
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,12 +3,16 @@ let mix = require('laravel-mix');
 mix.js('src/js/script.js', '.')
     .sass('src/scss/style.scss', '.').options({
         processCssUrls: false
+    })
+    .sass('src/scss/admin.scss', '.').options({
+        processCssUrls: false
     });
 
 if (mix.inProduction()) {
     mix.copy('./wp-content/plugins/cmb2', './vendor/cmb2/cmb2');
 
     mix.copy('./*.php', './dist')
+        .copy('./admin.css', './dist')
         .copy('./style.css', './dist')
         .copy('./script.js', './dist')
         .copy('./screenshot.png', './dist')
